2708a3625c19ddcbda45feedd835fc5715a079b5
Mônica Mendes <monicamendesmontanha@gmail.com>
2017-11-25 20:43:10 +1100

    node girls express workshop

--- server.js
+++ server.js
@@ -1,18 +1,33 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
+var formidable = require('express-formidable');
 
 app.use(express.static("public"));
-
-// app.get('/get-posts', function (req, res) {
-//     res.send('Hello there!');
-//     console.log('/get-posts')
-// });
-
-app.post('/create-post', function (req, res) {
-    res.send('Hello there!');
-    console.log('/create-post')
+app.use(formidable());
+app.get("/get-posts", function (request, response) { 
+    response.sendFile(__dirname + '/data/posts.json')
 });
 
-app.listen(3000, function() {
-  console.log('Server is listening on port 3000, Ready to accept requests!')
+app.post("/create-post", function (request, response) {
+
+    fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+        if (error) {
+            console.log("Desculpe, mas o sistema nao conseguiu escrever o seu arquivo, tente novamente mais tarde.");
+        }
+        
+        var parsedJson = JSON.parse(file);
+        var key = Date.now();
+        var value = request.fields.blogpost;
+
+        parsedJson[key] = value;
+        var data = JSON.stringify(parsedJson);
+        fs.writeFile(__dirname + '/data/posts.json', data, function (error) {
+        // do something
+        });
+    });
+});
+
+app.listen(3000,function () {
+ console.log('Server is listening on port 3000. Ready to accept requests!');  
 });
